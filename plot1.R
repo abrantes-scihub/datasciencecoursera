5167a41cd7c271a6ef88ea07eb52c1ddee7e9c5a
ds1800 <diego.jose.saa@gmail.com>
2015-06-05 15:18:45 -0500

    upload files for Exploratory Data Analysis Project 1

---
+++ plot1.R
@@ -0,0 +1,28 @@
+# EXPLORATORY DATA ANALYSIS
+# COURSE PROJECT 1
+# The dataset has 2,075,259 rows and 9 columns.
+# We will only be using data from the dates 2007-02-01 and 2007-02-02.
+# 
+file <- "~/data/household_power_consumption.csv"
+select <- "select * from file where Date in ('1/2/2007', '2/2/2007')"
+hpc <- read.csv.sql(file, sql = select) 
+
+times <- as.POSIXct(strptime(paste(hpc$Date, hpc$Time), "%d/%m/%Y %H:%M:%S"))
+hpc1 <- mutate(hpc, datetime = times) 
+
+# Our overall goal here is simply to examine how household energy usage 
+# varies over a 2-day period in February, 2007. Your task is to reconstruct 
+# the following plots below, all of which were constructed using the base 
+# plotting system.
+
+# PLOT 1:
+hist(hpc1$Global_active_power, col="red", 
+     main ="Global active power", xlab = "Global active power (kilowatts)")
+dev.copy(png, file="./plot1.png", width=480, height=480)
+dev.off()
+
+# Reproduce the image (within a different frame):
+library(png)
+img1 <- readPNG("./plot1.png")
+plot(c(0, 500), c(0, 500), type = "n", xlab = "", ylab = "")
+rasterImage(img1, 0, 0, 480, 480)
