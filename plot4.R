ea664d39606e242cfdd03a57ca072fb31a18e550
ds1800 <diego.jose.saa@gmail.com>
2015-06-06 17:03:37 -0500

    plot 4 and plot4.R updated

--- plot4.R
+++ plot4.R
@@ -7,15 +7,23 @@ file <- "~/data/household_power_consumption.csv"
 select <- "select * from file where Date in ('1/2/2007', '2/2/2007')"
 hpc <- read.csv.sql(file, sql = select) 
 
+# Second alternative (If dataset enters in memory)
+hpc <- read.csv(file)
+hpc1 <- sqldf(select)  ## I am not going to use this
+
 times <- as.POSIXct(strptime(paste(hpc$Date, hpc$Time), "%d/%m/%Y %H:%M:%S"))
 hpc1 <- mutate(hpc, datetime = times) 
+
 
 # Our overall goal here is simply to examine how household energy usage 
 # varies over a 2-day period in February, 2007. Your task is to reconstruct 
 # the following plots below, all of which were constructed using the base 
 # plotting system.
 
-# PLOT 4:
+# PLOT 4':
+## Since "dev.copy" truncates the rightmost two characters, then
+## send a copy directly to the plot3.png output file in disk:
+png("./plot4.png", width=480, height=480)
 par(mfcol = c(2, 2), mar = c(4.5, 4, 1.5, 1))
 with(hpc1, {
   plot(hpc1$datetime, hpc1$Global_active_power, type ="l", xlab="", ylab = "Global active power (kilowatts)") 
@@ -30,7 +38,6 @@ with(hpc1, {
   
   plot(hpc1$datetime, hpc1$Global_reactive_power, type ="l", xlab="datetime", ylab = "Global_reactive_power")
 })
-dev.copy(png, file="./plot4.png", width=480, height=480)
 dev.off()
 
 # Reproduce the image (within a different frame):
